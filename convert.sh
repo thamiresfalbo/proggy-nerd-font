4d1938643d717092eaa8dc65e5311a7690dd3b58
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2022-11-19 14:42:12 -0300

    .

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'Finding fonts... This might take some time.'
-fd . $PWD/proggyfonts -e ttf --exec fontforge --script nerd-fonts/font-patcher -c -out dist/ {} || "Error!" && exit
+fd . $PWD/proggyfonts -e ttf --exec fontforge --script nerd-fonts/font-patcher -c -l -s -out output/ {} || "Error!" && exit
