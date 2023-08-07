631dfdf35627613471b95caee96973ee85edfe29
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2023-08-06 21:44:17 -0300

    Maybe it will work now with find

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'Finding fonts... This might take some time.'
-fd . $PWD/proggyfonts -e ttf --exec fontforge --script ./FontPatcher/font-patcher -c -l -out output/ {} || "Error!" && exit
+find . $PWD/proggyfonts/ -name "*.ttf" -exec fontforge FontPatcher/font-patcher -c -l -out output/ {} \; || "Error!" && exit
