1722056e7ef4271c5d101bcc74440485c3af3721
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2023-08-06 21:25:08 -0300

    Changed Github Workflow.

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'Finding fonts... This might take some time.'
-fd . $PWD/proggyfonts -e ttf --exec fontforge --script nerd-fonts/FontPatcher/font-patcher -c -l -s -out output/ {} || "Error!" && exit
+fd . $PWD/proggyfonts -e ttf --exec fontforge --script ./FontPatcher/font-patcher -c -l -out output/ {} || "Error!" && exit
